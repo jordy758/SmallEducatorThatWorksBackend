a478bd9136a9d588038c77d55b8530ad069ea236
JdeJong <jeroen--j@hotmail.com>
2019-03-26 18:54:37 +0100

    still don't know what im doing wrong

--- resources/views/lesson/show.blade.php
+++ resources/views/lesson/show.blade.php
@@ -12,7 +12,7 @@
                         <hr />
                         <a href="{{ route('show_play_list', ['playList' => $lesson->playList]) }}">Back to playlist</a>
                         <hr/>
-                        <a href="{{ route('delete_lesson', ['lesson_id' => $lesson->id]) }}" class="btn btn-primary">Delete lesson</a>&nbsp;&nbsp;
+                        <a href="{{ route('delete_lesson', ['lesson' => $lesson]) }}" class="btn btn-primary">Delete lesson</a>&nbsp;&nbsp;
                     </div>
                 </div>
             </div>
